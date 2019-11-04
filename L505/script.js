4f3971af0e8f5a79902cd28eff7837c0cba05635
ttskhadiashvili <56174408+ttskhadiashvili@users.noreply.github.com>
2019-11-04 16:07:19 +0400

    laptop / desktop

---
+++ L505/script.js
@@ -0,0 +1,12 @@
+let x = prompt ('რისი ყიდვა გსურთ?');
+
+
+if (x === 'desktop')
+{
+    document.querySelector(".desktop").style.backgroundColor = "blue";
+}
+
+ if (x === 'laptop')
+{
+    document.querySelector(".latop").style.backgroundColor = "green";
+}
