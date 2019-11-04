c1a5f0e9ef58bb1361be57d3c4e2c9db9a81c40a
ttskhadiashvili <56174408+ttskhadiashvili@users.noreply.github.com>
2019-11-04 16:10:35 +0400

    update

--- L505/script.js
+++ L505/script.js
@@ -6,7 +6,30 @@ if (x === 'desktop')
     document.querySelector(".desktop").style.backgroundColor = "blue";
 }
 
- if (x === 'laptop')
+else if (x === 'laptop')
 {
     document.querySelector(".latop").style.backgroundColor = "green";
 }
+
+
+//მეორე ვარიანტი 
+
+// if (x = 'desktop')
+// {
+//     var d, i; 
+//     d =  document.querySelectorAll(".desktop-item");
+//     for (i = 0; i < d.length; i++)
+//     {
+//         d[i].style.backgroundColor = "blue";
+//     }
+// }
+
+// else if (x = 'laptop')
+// {
+//     var l, i; 
+//     l =  document.querySelectorAll(".latop-item");
+//     for (i = 0; i < l.length; i++)
+//     {
+//         l[i].style.backgroundColor = "green";
+//     }
+// }
