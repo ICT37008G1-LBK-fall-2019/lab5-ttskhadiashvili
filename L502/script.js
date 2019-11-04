a9d58d491f01ae3d053f65dc7995c5749047dcda
ttskhadiashvili <56174408+ttskhadiashvili@users.noreply.github.com>
2019-11-04 15:38:23 +0400

    ლაბ 5

---
+++ L502/script.js
@@ -0,0 +1,18 @@
+
+if (document.getElementById('users').children.length === 0) {
+
+    alert('მომხმარებლები - ცარიელია')
+}
+else {
+    alert('მომხმარებლები - სავსეა')
+}
+
+
+
+if (document.getElementById('empty-list').children.length === 0) {
+
+    alert('ცარიელი სია - ცარიელია')
+}
+else {
+    alert('ცარიელი სია - სავსეა')
+}
---
+++ L504/script.js
@@ -0,0 +1,7 @@
+var p, i;
+p = document.querySelectorAll('.model');
+
+for (i = 0; i < p.length; i++) {
+    alert(p[i].textContent);
+}
+
